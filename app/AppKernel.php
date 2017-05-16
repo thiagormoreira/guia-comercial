7dc9ce06c54d465c44be7c2b7ff54299796d7b4c
Thiago Moreira <loganguns@gmail.com>
2017-05-16 05:15:40 -0300

    LoadFixtures Adicionado FontAwesome Adicioando doctrine load fixtures para heroku deploy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] = new Webonaute\DoctrineFixturesGeneratorBundle\DoctrineFixturesGeneratorBundle();
         }
 
         return $bundles;
